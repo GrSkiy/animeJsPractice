df35b7abbb2a58ed6223472f0255ae43fe3597c4
GrSkiy <43372592+GrSkiy@users.noreply.github.com>
2019-03-04 14:10:22 +0300

    2.2

--- assets/JS/script2.js
+++ assets/JS/script2.js
@@ -25,8 +25,144 @@ animation
   targets: '.glaz',
   opacity: 1,
   duration: 500,
+})
+.add({
+  targets: '.word_box1',
+  translateY: 200,
+  duration: 300,
+})
+.add({
+targets: '.word_box2',
+translateY: 200,
+duration: 300,
+})
+.add({
+targets: '.word_box3',
+translateY: 200,
+duration: 300,
 });
 
+anime({
+  targets: ".glaz",
+  top: 260,
+  direction: 'alternate',
+  loop: true,
+})
 
-document.querySelector('.play').onclick = animation.play;
-document.querySelector('.stop').onclick = animation.pause;
+let animation2 = anime.timeline({
+autoplay:false,
+});
+
+animation2
+.add({
+  targets: ".glaz",
+  opacity: 0,
+  duration: 800,
+})
+.add({
+  targets: ".red",
+  opacity: 0,
+  duration: 800,
+})
+.add({
+  targets: ".word_box1, .word_box2, .word_box3",
+  opacity: 0,
+  duration: 800,
+})
+.add({
+  targets: ".large_letter1",
+  translateY: 400,
+  duration:300
+})
+.add({
+  targets: ".large_letter2",
+  translateY: 400,
+  duration:400
+})
+.add({
+  targets: ".large_letter3",
+  translateY: 400,
+  duration:550
+})
+.add({
+  targets: ".large_letter4",
+  translateY: 400,
+  duration:350
+})
+.add({
+  targets: ".large_letter5",
+  translateY: 400,
+  duration:300
+})
+.add({
+  targets: ".large_letter6",
+  translateY: 400,
+  duration:380
+})
+.add({
+  targets: ".large_letter7",
+  translateY: 400,
+  duration: 500
+})
+.add({
+  targets: ".large_letter8",
+  translateY: 400,
+  duration: 400
+})
+.add({
+  targets: ".large_letter9",
+  translateY: 400,
+  duration:600
+})
+.add({
+  targets: ".large_letter10",
+  translateY: 400,
+  duration:300
+})
+.add({
+  targets: ".large_letter12",
+  translateY: 400,
+  duration:300
+})
+.add({
+  targets: ".large_letter13",
+  translateY: 400,
+  duration:300
+})
+.add({
+  targets: ".large_letter14",
+  translateY: 400,
+  duration:300
+})
+.add({
+  targets: ".large_letter15",
+  translateY: 400,
+  duration:300
+})
+.add({
+  targets: ".large_letter16",
+  translateY: 400,
+  duration:350
+})
+.add({
+  targets: ".large_letter17",
+  translateY: 400,
+  duration:400
+})
+.add({
+  targets: ".large_letter18",
+  translateY: 400,
+  duration:400
+})
+
+// let animation2 = anime.timeline({
+//   autoplay: false,
+//
+//   });
+//
+//   animation2
+//
+
+
+
+document.querySelector('.red').onclick = animation2.restart;
